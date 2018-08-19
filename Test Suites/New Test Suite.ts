160fc24f0f0b732ee348157899b23ef49897ef14
Vinh Nguyen <vinhnguyen@kms-technology.com>
2018-08-19 16:40:37 +0700

    Add temp Test Case

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-19T16:37:18</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4bf8c9c8-11fc-4b42-8670-e272d0a6d4b5</testSuiteGuid>
+   <testCaseLink>
+      <guid>72f3731a-5e23-448c-8496-279bcc497d08</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
